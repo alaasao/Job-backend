3133fee797c92622ebb347d8f33d9acf0e95bfe4
alaasao <a_souchi@estin.dz>
2024-06-23 22:57:39 +0100

    still

--- app.js
+++ app.js
@@ -2,25 +2,25 @@ const express = require("express")
 require("express-async-errors")
 const app = express()
 app.use(express.json())
-const helmet = require("helmet")
-const xss = require("xss-clean")
-const cors = require("cors")
-const rateLimiter=require("express-rate-limit")
+
+// const xss = require("xss-clean")
+// const cors = require("cors")
+// const rateLimiter=require("express-rate-limit")
 require("dotenv/config")
 const jobsRouter = require("./routes/jobs")
 const notFound = require('./middleware/not-found.js')
 const errorHandlerMiddleware = require("./middleware/error-handler")
 const authRouter = require("./routes/auth.js")
 const auth = require("./middleware/authentication.js")
-app.set('trust proxy',1)
-const limiter = rateLimiter({
-    windowMs: 15 * 60 * 1000,
-    max:100
-})
-app.use(limiter)
-app.use(cors())
-app.use(helmet())
-app.use(xss())
+// app.set('trust proxy',1)
+// const limiter = rateLimiter({
+//     windowMs: 15 * 60 * 1000,
+//     max:100
+// })
+// app.use(limiter)
+// app.use(cors())
+// app.use(helmet())
+// app.use(xss())
 app.get('/', (req, res) => {
     res.send("it worked")
 })
